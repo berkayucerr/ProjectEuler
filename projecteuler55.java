46c217c562c30183cc475f22309392aa99c71f2a
berkayucerr <berkayucerr@gmail.com>
2021-02-17 22:01:53 +0300

    Initial commit

---
+++ projecteuler55.java
@@ -0,0 +1,27 @@
+public class projecteuler55
+{
+
+    projecteuler55(String x){
+        int temp;
+        int varInteger=Integer.parseInt(x);
+        for (int i = 0; i <varInteger ; i++) {
+            temp=(reverse(i)+i);
+            if(reverse(temp)==temp){
+                System.out.println(i+ " is a palindrome");
+            }
+        }
+
+    }
+    private int reverse(int x) {
+        String a=new String() ;
+        a=String.valueOf(x);
+        byte[] strAsByteArray = a.getBytes();
+        byte[] result = new byte[strAsByteArray.length];
+
+        for (int i = 0; i < strAsByteArray.length; i++)
+            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+
+        return (Integer.parseInt(new String(result)));
+    }
+
+}
